6107e7e7c75958b67a8f63dd0c9a4aab2fb18137
Animalien <andy.wright@gmail.com>
2015-11-23 23:29:24 -0800

    Created SoundSys, first inits
    SoundSys does CoInitialize and CoCreateInstance calls, reports any
    errors

--- SoundTest/SoundTest.cpp
+++ SoundTest/SoundTest.cpp
@@ -2,6 +2,10 @@
 
 #include <windows.h>
 
+#include <Audioclient.h>
+#include <Mmdeviceapi.h>
+#include <Objbase.h>
+
 #include <fcntl.h>
 #include <io.h>
 #include <stdio.h>
@@ -24,14 +28,105 @@ bool InitConsole()
 }
 
 
-void PrintLastWinError()
+void PrintWinError(DWORD err)
 {
 	char buff[2048];
 
-	FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM, nullptr, GetLastError(), 0, buff, sizeof(buff), nullptr);
+	FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM|FORMAT_MESSAGE_IGNORE_INSERTS, nullptr, err, 0, buff, sizeof(buff), nullptr);
 	printf("%s", buff);
 }
 
+void PrintLastWinError()
+{
+	PrintWinError(GetLastError());
+}
+
+class MyHResult
+{
+public:
+	MyHResult() : m_hr(0)  { }
+
+	void operator= (HRESULT hr) { m_hr = hr; }
+	bool Failed() const { return FAILED(m_hr); }
+	void Print(const char* prefix = nullptr)
+	{
+		if (prefix)
+			printf("%s:  ", prefix);
+		printf("HResult = 0x%08X:  ", (unsigned)m_hr);
+		PrintWinError(m_hr);
+	}
+
+private:
+	HRESULT m_hr;
+};
+
+MyHResult myhr;
+
+////////////////////////////////////////////////////////////////////////////////
+
+class SoundSys
+{
+public:
+	SoundSys();
+
+	bool InitSucceeded() const { return m_initSucceeded; }
+
+private:
+	bool m_initSucceeded;
+};
+
+
+#define REFTIMES_PER_SEC  10000000
+#define REFTIMES_PER_MILLISEC  10000
+
+SoundSys::SoundSys()
+: m_initSucceeded(false)
+{
+	const CLSID CLSID_MMDeviceEnumerator = __uuidof(MMDeviceEnumerator);
+	const IID IID_IMMDeviceEnumerator = __uuidof(IMMDeviceEnumerator);
+	const IID IID_IAudioClient = __uuidof(IAudioClient);
+	const IID IID_IAudioRenderClient = __uuidof(IAudioRenderClient);
+
+	REFERENCE_TIME hnsRequestedDuration = REFTIMES_PER_SEC;
+//	REFERENCE_TIME hnsActualDuration;
+	IMMDeviceEnumerator *pEnumerator = NULL;
+	IMMDevice *pDevice = NULL;
+	IAudioClient *pAudioClient = NULL;
+	IAudioRenderClient *pRenderClient = NULL;
+	WAVEFORMATEX *pwfx = NULL;
+//	UINT32 bufferFrameCount;
+//	UINT32 numFramesAvailable;
+//	UINT32 numFramesPadding;
+//	BYTE *pData;
+//	DWORD flags = 0;
+
+	myhr = CoInitializeEx(
+				nullptr,
+				COINIT_MULTITHREADED | COINIT_SPEED_OVER_MEMORY);
+	if (myhr.Failed())
+	{
+		myhr.Print("CoInitializeEx");
+		return;
+	}
+
+	myhr = CoCreateInstance(
+				CLSID_MMDeviceEnumerator, NULL,
+				CLSCTX_ALL, IID_IMMDeviceEnumerator,
+				(void**)&pEnumerator);
+	if (myhr.Failed())
+	{
+		myhr.Print("CoCreateInstance");
+		return;
+	}
+
+
+
+	m_initSucceeded = true;
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////
 
 int CALLBACK WinMain(
 	_In_ HINSTANCE hInstance,
@@ -43,11 +138,12 @@ int CALLBACK WinMain(
 	if (!InitConsole())
 		return 0;
 
-	printf("Printing\n");
-	printf("Printing\n");
-	printf("Printing\n");
+	SoundSys sndSys;
+	if (sndSys.InitSucceeded())
+		printf("SoundSys Init succeeded!\n\n");
+	else
+		printf("SoundSys Init failed\n\n");
 
-	printf("Done!\n\n");
 	Sleep(5000);
 
 	return 0;
